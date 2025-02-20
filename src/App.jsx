c2bfda99538a3bd4ee06c07191bdc9a7ed23523e
NEVERTHELESSE <neverthelesse@localhost.localdomain>
2025-02-20 05:41:34 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import dev from "/logo.jpg";
+
+export default function App() {
+  return (
+    <div>
+      <p className="text-4xl text-orange-700">neverthelesse</p>
+      <img src={dev} alt="logo" />
+    </div>
+  );
+}
